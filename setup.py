037c5a75dd4a87834c0a53306ad780cd03fbed74
Jacek Łysiak <jacek.lysiako.o@gmail.com>
2017-12-17 10:42:19 +0100

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='CMS-NN',
+        version='1.0',
+        description='Neural Networks tools for CMS experiment',
+        author='Jacek Łysiak',
+        author_email='jaceklysiako.o@gmail.com',
+        packages=[],
+    )
